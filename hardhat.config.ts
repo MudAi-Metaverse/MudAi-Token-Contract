324b0666a161e2bed05ecd82ba40aa6cf878d3a8
Kenshiro-K <kenkikuti@icloud.com>
2023-02-01 03:07:45 +0900

    feat : chenged hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,24 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }, 
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.GOERLI_PRIVATE_KEY!]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_TOKEN
+  },
 };
 
 export default config;
